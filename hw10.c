0b796026303ec53a7cce0519023bc25a6c5d207a
macoo0906 <127402507+macoo0906@users.noreply.github.com>
2023-12-02 02:29:51 +0900

    Add files via upload

---
+++ hw10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef struct city
+{
+	char name[100];
+	char country[100];
+	int population;
+}City;
+
+int main0()
+{
+	City arr[3];
+	printf("Input three cities:\n");
+	for (int i = 0; i < 3; i++)
+	{
+		printf("Name> ");
+		scanf("%s", arr[i].name, sizeof(arr[i].name));
+		printf("Country> ");
+		scanf("%s", arr[i].country, sizeof(arr[i].country));
+		printf("Population> ");
+		scanf("%d", &arr[i].population);
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		printf("Printing the three cities:\n");
+		printf("%d. %s in %s with a population of %d people\n",i+1, arr[i].name, arr[i].country, arr[i].population);
+	}
+	return 0;
+}
